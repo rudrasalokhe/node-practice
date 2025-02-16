f21494debc35636f5389bd0f5b64422dea46197e
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-02-16 21:48:39 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function add(a, b){
-    return a+b;
+const { error } = require('console');
+const fs = require('fs');
+function main(filename){
+    fs.readFile(filename, "utf-8"  ,function(err, data){
+        let total = 0;
+        for(let i=0;i<data.length;i++){
+            console.log(data[i]);
+            if(data[i]= " "){
+                total++;
+            }
+        }
+        console.log(total);
+    })
 }
-const ans = add(1,2);
-console.log(ans);
-
-
-// 40.00 minutes cohort timing.
+main("a.txt");
